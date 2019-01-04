2540da563c49196f7e35a97a69cd1dcda0e884d0
IRGI FAHRIAS <fahrias600@gmail.com>
2019-01-04 15:46:17 +0700

    siswa

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -1,16 +1,14 @@
 @push('header-scripts')
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
-@endpush
 @extends('layouts.master')
 @section('title','SISWA')
 @section('content')
-<a class="btn btn-success" href="{{URL::to('siswa/create')}}">+ siswa</a>
-<br>
+<a class="btn btn-success fa fa-user" href="{{URL::to('siswa/create')}}">+</a>
 <div class="col-md-12 col-sm-12 col-xs-24">
     <div class="x_panel">
       <div class="x_title">
-        <h2>Daftar Siswa <small color="blue">MAGANG</small></h2>
+        <h2>Daftar Siswa <small color="blue">Magang</small></h2>
         <ul class="nav navbar-right panel_toolbox">
           <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
           </li>
@@ -46,35 +44,26 @@
             @foreach($siswa as $key => $value)
                 <tr>
 		    <th scope="row">{{$value->id}}</th>
-                    <td>{{$value->nama}}</td>
+                    <td>{{$value->nama}}</td>                 
                     <td>{{$value->jurusan}}</td>
                     <td>{{$value->kelas}}</td>
                     <td>{{$value->guru_id}}</td>
-                    <td>{{$value->pembimbing_id}}</td>                    
+                    <td>{{$value->pembimbing_id}}</td>
                     <td>
-                    <form action="{{ route('siswa.destroy', $value->id) }}" method="post">
+                    <form action="{{ route('pembimbing.destroy', $value->id) }}" method="post">
                         {{ csrf_field() }}
                         <input name="_method" type="hidden" value="DELETE">
                         <button class="btn btn-danger" type="submit"><i class="fa fa-trash"></i></button>
-                        <a href="{{route('siswa.edit', $value->id)}}" class="btn btn-warning"><i class="fa fa-edit"></i></a>
+                        <a href="{{route('pembimbing.edit', $value->id)}}" class="btn btn-warning"><i class="fa fa-edit"></i></a>
                     </form>
                 </td>
                 </tr>
             @endforeach
           </tbody>
         </table>
-
+      </div>
       </div>
     </div>
   </div>
 @endsection
-@push('scripts')
-  <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
-  <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
-  <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
-    <script>
-        $(document).ready(function(){
-            $('#tabel-data').DataTable();
-        });
-    </script>
 @endpush
