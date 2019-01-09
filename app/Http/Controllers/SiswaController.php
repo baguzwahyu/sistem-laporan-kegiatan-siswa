7ea1a1b403b2ac192d80d751d0531cce54c24b87
aryafirman <aryafirman40@gmail.com>
2019-01-09 15:33:59 +0700

    perusahaan controll

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -3,38 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\siswa;
+use App\Guru;
+use App\Pembimbing;
 use Validator;
+use DB;
 use Illuminate\Http\Request;
 
 class SiswaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function index()
     {
-        $siswa = siswa::all();
-        return view('siswa.index', compact('siswa'));
+        $siswa = siswa::with('guru')->get();
+        $siswa = siswa::with('pembimbing')->get();
+        return view('siswa.index',compact('siswa') );
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function create()
     {
-        return view('siswa.create');
+        $gurus = guru::all();
+        $pembimbings = pembimbing::all();
+        return view('siswa.create', compact('gurus','pembimbings'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    
     public function store(Request $request)
     {
         $this->Validate($request,[
@@ -52,39 +45,24 @@ class SiswaController extends Controller
             'pembimbing_id'=>$request->get('pembimbing_id'),
         ]);
         $siswa->save();
-        return redirect('/');
+        return redirect('/siswa');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\siswa  $siswa
-     * @return \Illuminate\Http\Response
-     */
+    
     public function show(siswa $siswa)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\siswa  $siswa
-     * @return \Illuminate\Http\Response
-     */
+   
     public function edit($id)
     {
        $siswa = siswa::findorfail($id);
+       
        return view('siswa.edit',compact('siswa'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\siswa  $siswa
-     * @return \Illuminate\Http\Response
-     */
+    
     public function update(Request $request,$siswa)
     {
         $siswa = siswa::findorfail($siswa);
@@ -92,12 +70,7 @@ class SiswaController extends Controller
         return redirect()->route('siswa.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\siswa  $siswa
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy(siswa $siswa)
     {
         $siswa->delete();
